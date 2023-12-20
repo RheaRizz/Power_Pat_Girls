7cb11a1204f0854d68ba6b0682c3f3a67e540b2f
Jokkeeerrz <jpp200214@gmail.com>
2023-12-20 13:46:00 +0800

    fixed login
    login now works just need to change the code for it to redirect to patient or doctor dash

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //ROUTES//
-app.post('/create-account', async (req, res) => {
+app.post('/create-account-success', async (req, res) => {
   const { newUserFirstName, newUserLastName, newUserEmail, newPassword, userType } = req.body;
   const hashedPassword = await bcrypt.hash(newPassword, 10);
   const newUser = {
@@ -41,16 +41,20 @@ app.post('/create-account', async (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-  app.get()
-  const { userEmail, userPassword } = req.body;
-  const user = pool.users.find(user => user.newUserEmail === userEmail);
+  const { email, password } = req.body;
+  const { rows: users } = await pool.query('SELECT * FROM USERS');
+  const user = users.find(user => user.email === email);
 
   if (user !== undefined) {
-    const passwordMatch = await bcrypt.compare(userPassword, user.userPassword);
-    if (passwordMatch) {
-      res.json({ message: 'Login successful' });
+    if (password && user.password) {
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (passwordMatch) {
+        res.json({ message: 'Login successful' }); // change logic for redirecting for either patient or doctor
+      } else {
+        res.status(401).json({ message: 'Invalid password'});
+      }
     } else {
-      res.status(401).json({ message: 'Invalid password'});
+      res.status(401).json({ message: 'Password is undefined'})
     }
   } else {
     res.status(401).json({ message: 'User not found' });
@@ -58,7 +62,15 @@ app.post('/login', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views', 'log_in.html'))
+  res.sendFile(path.join(__dirname, '../views', 'log_in.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views', 'log_in.html'));
+});
+
+app.get('/create-account', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views', '/create-acc.html'))
 })
 
 app.listen(port, () => {
